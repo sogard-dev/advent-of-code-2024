cc6b7b840428aa7cfb1087ee2024333f8fac585b
morten <morten.sogard.jensen@gmail.com>
2024-12-02 12:32:58 +0000

    day 2

--- advent-of-code-2024/day2/task.go
+++ advent-of-code-2024/day2/task.go
@@ -1,20 +1,56 @@
 package day2
 
 import (
-	"fmt"
+	"slices"
 	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2024/utils"
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
-	}
-	return 0
+	return solve(input, false)
 }
 
 func part2(input string) int {
+	return solve(input, true)
+}
+
+func solve(input string, allow bool) int {
+	safe := 0
 	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+		nums := utils.GetAllNumbers(line)
+		if isSafe(nums) {
+			safe++
+		} else if allow {
+			for idxToRemove := range nums {
+				a := []int{}
+				for k := range nums {
+					if k != idxToRemove {
+						a = append(a, nums[k])
+					}
+				}
+				if isSafe(a) {
+					safe++
+					break
+				}
+			}
+		}
+
 	}
-	return 0
+	return safe
+}
+
+func isSafe(nums []int) bool {
+	if nums[0] > nums[1] {
+		slices.Reverse(nums)
+	}
+
+	for i := 0; i < len(nums)-1; i++ {
+		d := nums[i+1] - nums[i]
+
+		if d < 1 || d > 3 {
+			return false
+		}
+	}
+	return true
 }
--- advent-of-code-2024/day2/task_test.go
+++ advent-of-code-2024/day2/task_test.go
@@ -7,12 +7,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const testInput = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 2, part1(testInput))
+	require.Equal(t, 559, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 4, part2(testInput))
+	require.Equal(t, 601, part2(utils.GetInput(t, "input.txt")))
 }
