e9fc8e0051de9c92a5e431c60cfcec6f0bff0f24
morten <morten.sogard.jensen@gmail.com>
2024-12-02 12:01:56 +0000

    day 1

--- advent-of-code-2024/day1/task.go
+++ advent-of-code-2024/day1/task.go
@@ -1,20 +1,54 @@
 package day1
 
 import (
-	"fmt"
-	"strings"
+	"slices"
+
+	"github.com/sogard-dev/advent-of-code-2024/utils"
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	l1, l2 := parseLists(input)
+
+	d := 0
+	for i := range l1 {
+		d += abs(l1[i] - l2[i])
 	}
-	return 0
+
+	return d
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	l1, l2 := parseLists(input)
+
+	occ := map[int]int{}
+	for _, n := range l2 {
+		occ[n] += 1
 	}
-	return 0
+
+	s := 0
+	for _, n := range l1 {
+		s += n * occ[n]
+	}
+	return s
+}
+
+func parseLists(input string) ([]int, []int) {
+	l1, l2 := []int{}, []int{}
+	for i, n := range utils.GetAllNumbers(input) {
+		if i%2 == 0 {
+			l1 = append(l1, n)
+		} else {
+			l2 = append(l2, n)
+		}
+	}
+	slices.Sort(l1)
+	slices.Sort(l2)
+	return l1, l2
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return -i
+	}
+	return i
 }
--- advent-of-code-2024/day1/task_test.go
+++ advent-of-code-2024/day1/task_test.go
@@ -7,12 +7,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const testInput = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
+
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 11, part1(testInput))
+	require.Equal(t, 1603498, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
+	require.Equal(t, 31, part2(testInput))
 	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
 }
