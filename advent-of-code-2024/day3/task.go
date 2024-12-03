d162178f7f5f2fc9ea6d0c483875b4396c71fb0a
morten <morten.sogard.jensen@gmail.com>
2024-12-03 06:30:48 +0000

    day 3

--- advent-of-code-2024/day3/task.go
+++ advent-of-code-2024/day3/task.go
@@ -1,20 +1,55 @@
 package day3
 
 import (
-	"fmt"
-	"strings"
+	"regexp"
+
+	"github.com/sogard-dev/advent-of-code-2024/utils"
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	r, _ := regexp.Compile(`mul\((\d{1,3}),(\d{1,3}\))`)
+
+	sum := 0
+	for _, m := range r.FindAllString(input, -1) {
+		nums := utils.GetAllNumbers(m)
+		sum += nums[0] * nums[1]
 	}
-	return 0
+
+	return sum
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	do, _ := regexp.Compile(`do\(\)`)
+	dont, _ := regexp.Compile(`don't\(\)`)
+
+	enableIdx := do.FindAllStringIndex(input, -1)
+	disableIdx := dont.FindAllStringIndex(input, -1)
+	enabled := map[int]bool{}
+
+	for _, v := range enableIdx {
+		enabled[v[0]] = true
 	}
-	return 0
+
+	for _, v := range disableIdx {
+		for k := v[0]; k < len(input); k++ {
+			if _, exists := enabled[k]; !exists {
+				enabled[k] = false
+			} else if enabled[k] {
+				break
+			}
+		}
+	}
+
+	sum := 0
+	mul, _ := regexp.Compile(`mul\((\d{1,3}),(\d{1,3}\))`)
+	for _, m := range mul.FindAllStringIndex(input, -1) {
+		enabled, exists := enabled[m[0]]
+		t := input[m[0]:m[1]]
+		if !exists || enabled {
+			nums := utils.GetAllNumbers(t)
+			sum += nums[0] * nums[1]
+		}
+	}
+
+	return sum
 }
--- advent-of-code-2024/day3/task_test.go
+++ advent-of-code-2024/day3/task_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 161, part1(`xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`))
+	require.Equal(t, 161085926, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 48, part2(`xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`))
+	require.Equal(t, 82045421, part2(utils.GetInput(t, "input.txt")))
 }
