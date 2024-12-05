d7e3a536ec2cd0b17a895024f85c8f4046b1cb40
morten <morten.sogard.jensen@gmail.com>
2024-12-05 06:27:37 +0000

    day 5

--- advent-of-code-2024/day5/task.go
+++ advent-of-code-2024/day5/task.go
@@ -1,20 +1,80 @@
 package day5
 
 import (
-	"fmt"
+	"slices"
 	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2024/utils"
 )
 
+type rule struct {
+	a, b int
+}
+
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
-	}
-	return 0
+	return solve(input, false)
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	return solve(input, true)
+}
+
+func fixOrdering(rules []rule, pages []int) {
+	changed := true
+	for changed {
+		changed = false
+		for _, rule := range rules {
+			aIdx := slices.Index(pages, rule.a)
+			bIdx := slices.Index(pages, rule.b)
+			if aIdx != -1 && bIdx != -1 && bIdx < aIdx {
+				pages[aIdx], pages[bIdx] = pages[bIdx], pages[aIdx]
+				changed = true
+			}
+		}
 	}
-	return 0
+}
+
+func solve(input string, part2 bool) int {
+	blocks := strings.Split(input, "\n\n")
+	rulesNumbers := utils.GetAllNumbers(blocks[0])
+	rules := []rule{}
+	for i := 0; i < len(rulesNumbers); i += 2 {
+		rules = append(rules, rule{
+			a: rulesNumbers[i],
+			b: rulesNumbers[i+1],
+		})
+	}
+
+	sum := 0
+	for _, line := range strings.Split(blocks[1], "\n") {
+		pages := utils.GetAllNumbers(line)
+		initiallyValid := checkIsValid(rules, pages)
+		if !part2 {
+			if initiallyValid {
+				middlePage := pages[len(pages)/2]
+				sum += middlePage
+			}
+		} else {
+			if !initiallyValid {
+				fixOrdering(rules, pages)
+				middlePage := pages[len(pages)/2]
+				sum += middlePage
+			}
+		}
+	}
+
+	return sum
+}
+
+func checkIsValid(rules []rule, pages []int) bool {
+	valid := true
+	for _, rule := range rules {
+		aIdx := slices.Index(pages, rule.a)
+		bIdx := slices.Index(pages, rule.b)
+		if aIdx != -1 && bIdx != -1 && bIdx < aIdx {
+			valid = false
+			break
+		}
+	}
+	return valid
 }
