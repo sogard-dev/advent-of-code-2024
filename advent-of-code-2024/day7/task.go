82ddfa2aa68df5e8208e2ae6053fe012d485689b
morten <morten.sogard.jensen@gmail.com>
2024-12-07 07:21:02 +0000

    day 7

--- advent-of-code-2024/day7/task.go
+++ advent-of-code-2024/day7/task.go
@@ -1,20 +1,48 @@
 package day7
 
 import (
-	"fmt"
+	"strconv"
 	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2024/utils"
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
-	}
-	return 0
+	return solve(input, false)
 }
 
 func part2(input string) int {
+	return solve(input, true)
+}
+
+func solve(input string, withConcat bool) int {
+	res := 0
 	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+		nums := utils.GetAllNumbers(line)
+		sum := nums[0]
+		values := nums[1:]
+
+		if canBeTrue(sum, values, 0, withConcat) {
+			res += sum
+		}
 	}
-	return 0
+	return res
+}
+
+func canBeTrue(sum int, values []int, subsum int, withConcat bool) bool {
+	if len(values) == 0 {
+		return sum == subsum
+	} else {
+		c := canBeTrue(sum, values[1:], subsum+values[0], withConcat)
+		c = c || canBeTrue(sum, values[1:], subsum*values[0], withConcat)
+		if withConcat {
+			c = c || canBeTrue(sum, values[1:], concat(subsum, values[0]), withConcat)
+		}
+		return c
+	}
+}
+
+func concat(cur, next int) int {
+	a, _ := strconv.Atoi(strconv.Itoa(cur) + strconv.Itoa(next))
+	return a
 }
--- advent-of-code-2024/day7/task_test.go
+++ advent-of-code-2024/day7/task_test.go
@@ -8,11 +8,21 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 3749, part1(testInput))
+	require.Equal(t, 2437272016585, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 11387, part2(testInput))
+	require.Equal(t, 162987117690649, part2(utils.GetInput(t, "input.txt")))
 }
+
+const testInput = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`
