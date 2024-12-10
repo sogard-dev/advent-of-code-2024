499d3d1b35dab8d2a97a1bfd9eb4b22d95277351
moj <morten.sogard.jensen@systematic.com>
2024-12-10 09:29:54 +0000

    day 10

--- advent-of-code-2024/day10/task.go
+++ advent-of-code-2024/day10/task.go
@@ -1,20 +1,56 @@
 package day10
 
 import (
-	"fmt"
 	"strings"
 )
 
+type pos struct {
+	x, y int
+}
+
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
-	}
-	return 0
+	return solve(input, true)
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	return solve(input, false)
+}
+
+func solve(input string, cached bool) int {
+	sum := 0
+	lines := strings.Split(input, "\n")
+	for y, line := range lines {
+		for x, r := range line {
+			if r == '0' {
+				fromThis := routesFrom(lines, x, y, byte(r), map[pos]bool{}, cached)
+				sum += fromThis
+			}
+		}
 	}
-	return 0
+	return sum
+}
+
+func routesFrom(line []string, x, y int, cur byte, walked map[pos]bool, cached bool) int {
+	if cached {
+		if walked[pos{x: x, y: y}] {
+			return 0
+		}
+		walked[pos{x: x, y: y}] = true
+	}
+
+	if line[y][x] == '9' {
+		return 1
+	}
+
+	opt := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+	routes := 0
+	for _, d := range opt {
+		nx := x + d[0]
+		ny := y + d[1]
+		if nx >= 0 && ny >= 0 && ny < len(line) && nx < len(line[0]) && line[ny][nx] == cur+1 {
+			routes += routesFrom(line, nx, ny, cur+1, walked, cached)
+		}
+	}
+
+	return routes
 }
--- advent-of-code-2024/day10/task_test.go
+++ advent-of-code-2024/day10/task_test.go
@@ -8,11 +8,24 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 4, part1(`..90..9
+...1.98
+...2..7
+6543456
+765.987
+876....
+987....`))
+	require.Equal(t, 841, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 81, part2(`89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`))
+	require.Equal(t, 1875, part2(utils.GetInput(t, "input.txt")))
 }
