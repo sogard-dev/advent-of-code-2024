0aff80a85051f1c680827e63eb73ce0ee9e62043
moj <morten.sogard.jensen@systematic.com>
2024-12-13 09:01:21 +0000

    day 13

--- advent-of-code-2024/day13/task.go
+++ advent-of-code-2024/day13/task.go
@@ -1,20 +1,84 @@
 package day13
 
 import (
-	"fmt"
 	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2024/utils"
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
-	}
-	return 0
+	return solve(input, 0)
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	return solve(input, 10000000000000)
+
+}
+
+func solve(input string, add int) int {
+	blocks := strings.Split(strings.ReplaceAll(input, "\r", ""), "\n\n")
+
+	tokens := 0
+	for _, b := range blocks {
+		nums := utils.GetAllNumbers(b)
+		tokens += solveMachine(nums, add)
 	}
-	return 0
+
+	return tokens
+}
+
+func solveMachine(nums []int, add int) int {
+	targetX := nums[4] + add
+	targetY := nums[5] + add
+	ax := nums[0]
+	ay := nums[1]
+	bx := nums[2]
+	by := nums[3]
+
+	ci := 0
+	cj := min(targetX/bx, targetY/by)
+
+	calcX := func() int {
+		return ci*ax + cj*bx
+	}
+	calcY := func() int {
+		return ci*ay + cj*by
+	}
+
+	for {
+		cx := calcX()
+		cy := calcY()
+		dx := cx - targetX
+		dy := cy - targetY
+		if dx == 0 && dy == 0 {
+			return ci*3 + cj
+		}
+
+		rmJ := -1
+		adI := -1
+
+		if dx > 0 {
+			rmJ = max(rmJ, dx/bx)
+		}
+		if dy > 0 {
+			rmJ = max(rmJ, dy/by)
+		}
+		if rmJ >= 0 {
+			cj -= max(rmJ, 1)
+		}
+
+		if dx < 0 {
+			adI = max(adI, -dx/ax)
+		}
+		if dy < 0 {
+			adI = max(adI, -dy/ay)
+		}
+		if adI >= 0 {
+			ci += max(adI, 1)
+		}
+
+		if cj < 0 {
+			return 0
+		}
+	}
 }
--- advent-of-code-2024/day13/task_test.go
+++ advent-of-code-2024/day13/task_test.go
@@ -8,11 +8,24 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 480, part1(`Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279`))
+	require.Equal(t, 26299, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 107824497933339, part2(utils.GetInput(t, "input.txt")))
 }
