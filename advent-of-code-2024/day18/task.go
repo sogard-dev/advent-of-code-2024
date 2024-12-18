706dff0e11205efb61957a861946078c5d9633cf
moj <morten.sogard.jensen@systematic.com>
2024-12-18 10:06:53 +0000

    day 18

--- advent-of-code-2024/day18/task.go
+++ advent-of-code-2024/day18/task.go
@@ -1,20 +1,65 @@
 package day18
 
 import (
-	"fmt"
+	"math"
 	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2024/utils"
 )
 
-func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+type pos = utils.Coordinate2D
+
+func part1(input string, drops, ex, ey int) int {
+	blocked := map[pos]bool{}
+	lines := strings.Split(input, "\n")
+	for i := range drops {
+		n := utils.GetAllNumbers(lines[i])
+		x, y := n[0], n[1]
+		blocked[pos{X: x, Y: y}] = true
 	}
-	return 0
+
+	open := []pos{{X: 0, Y: 0}}
+	closed := map[pos]int{{X: 0, Y: 0}: 0}
+
+	shortest := math.MaxInt
+	ep := pos{X: ex, Y: ey}
+	for len(open) > 0 {
+		e := open[0]
+		dist := closed[e]
+		open = open[1:]
+
+		if e == ep && shortest > dist {
+			shortest = dist
+		}
+
+		for _, d := range utils.Get2DDirections() {
+			np := e.Add(d)
+			if np.X >= 0 && np.X <= ex && np.Y >= 0 && np.Y <= ey {
+				if _, e := closed[np]; !e && !blocked[np] {
+					open = append(open, np)
+					closed[np] = dist + 1
+				}
+			}
+		}
+	}
+	return shortest
 }
 
-func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+func part2(input string, ex, ey int) []int {
+	lines := strings.Split(input, "\n")
+
+	low, high := 0, len(lines)
+	for high-low > 1 {
+		mid := low + (high-low)/2
+
+		v := part1(input, mid, ex, ey)
+		if v == math.MaxInt {
+			high = mid
+		} else {
+			low = mid
+		}
 	}
-	return 0
+
+	n := utils.GetAllNumbers(lines[low])
+	return []int{n[0], n[1]}
 }
--- advent-of-code-2024/day18/task_test.go
+++ advent-of-code-2024/day18/task_test.go
@@ -8,11 +8,37 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 22, part1(testInput, 12, 6, 6))
+	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt"), 1024, 70, 70))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, []int{6, 1}, part2(testInput, 6, 6))
+	require.Equal(t, []int{46, 28}, part2(utils.GetInput(t, "input.txt"), 70, 70))
 }
+
+const testInput = `5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0`
