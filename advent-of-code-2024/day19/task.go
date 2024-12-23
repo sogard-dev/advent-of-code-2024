b59e09db2df602341185829e6ff7d8004a99247c
morten <morten.sogard.jensen@gmail.com>
2024-12-23 08:49:49 +0000

    day 19

--- advent-of-code-2024/day19/task.go
+++ advent-of-code-2024/day19/task.go
@@ -1,20 +1,59 @@
 package day19
 
 import (
-	"fmt"
 	"strings"
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	available, lines := parse(input)
+	s := 0
+	for _, line := range lines {
+		if canMake(available, line) > 0 {
+			s += 1
+		}
 	}
-	return 0
+	return s
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	available, lines := parse(input)
+	s := 0
+	for _, line := range lines {
+		s += canMake(available, line)
 	}
-	return 0
+	return s
+}
+
+func parse(input string) ([]string, []string) {
+	blocks := strings.Split(input, "\n\n")
+	available := strings.Split(blocks[0], ", ")
+	lines := strings.Split(blocks[1], "\n")
+	return available, lines
+}
+
+func canMake(available []string, line string) int {
+	cache := map[int]int{}
+	var check func(int) int
+	check = func(idx int) int {
+		if idx == len(line) {
+			return 1
+		}
+		if v, e := cache[idx]; e {
+			return v
+		}
+
+		canDo := 0
+		for _, a := range available {
+			la := len(a)
+			if la+idx <= len(line) && line[idx:idx+la] == a {
+				canDo += check(idx + la)
+			}
+		}
+
+		cache[idx] = canDo
+
+		return canDo
+	}
+
+	return check(0)
 }
--- advent-of-code-2024/day19/task_test.go
+++ advent-of-code-2024/day19/task_test.go
@@ -8,11 +8,22 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 6, part1(testInput))
+	require.Equal(t, 358, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 16, part2(testInput))
+	require.Equal(t, 600639829400603, part2(utils.GetInput(t, "input.txt")))
 }
+
+const testInput = `r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb`
