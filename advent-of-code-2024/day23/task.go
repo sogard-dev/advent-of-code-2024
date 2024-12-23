feaf7e4e0cdc89734e18a7225880932c4a8f6e03
morten <morten.sogard.jensen@gmail.com>
2024-12-23 08:23:26 +0000

    day 23

--- advent-of-code-2024/day23/task.go
+++ advent-of-code-2024/day23/task.go
@@ -1,20 +1,113 @@
 package day23
 
 import (
-	"fmt"
+	"slices"
 	"strings"
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	connections := parse(input)
+	clusters := findSetsOfThree(connections)
+
+	sum := 0
+	for _, v := range clusters {
+		hasT := false
+		for _, l := range v {
+			if l[0] == 't' {
+				hasT = true
+				break
+			}
+		}
+
+		if hasT {
+			sum += 1
+		}
 	}
-	return 0
+
+	return sum
 }
 
-func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+func part2(input string) string {
+	connections := parse(input)
+
+	longest := ""
+
+	for initK := range connections {
+		currentCluster := map[string]bool{initK: true}
+
+		changed := true
+		for changed {
+			changed = false
+			for k := range currentCluster {
+				for _, otherK := range connections[k] {
+					if !currentCluster[otherK] {
+						connectedToAll := true
+
+						for testK := range currentCluster {
+							if !slices.Contains(connections[otherK], testK) {
+								connectedToAll = false
+								break
+							}
+						}
+
+						if connectedToAll {
+							currentCluster[otherK] = true
+							changed = true
+						}
+					}
+
+				}
+			}
+		}
+
+		sl := []string{}
+		for k := range currentCluster {
+			sl = append(sl, k)
+		}
+		slices.Sort(sl)
+		str := strings.Join(sl, ",")
+		if len(str) > len(longest) {
+			longest = str
+		}
 	}
-	return 0
+
+	return longest
+}
+
+func parse(input string) map[string][]string {
+	connections := map[string][]string{}
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		a, b := line[0:2], line[3:5]
+		if _, e := connections[a]; !e {
+			connections[a] = []string{}
+		}
+		if _, e := connections[b]; !e {
+			connections[b] = []string{}
+		}
+		connections[a] = append(connections[a], b)
+		connections[b] = append(connections[b], a)
+	}
+	return connections
+}
+
+func findSetsOfThree(m map[string][]string) map[string][]string {
+	clusters := map[string][]string{}
+
+	for k1, c := range m {
+		for _, k2 := range c {
+			for _, k3 := range m[k2] {
+				for _, k4 := range m[k3] {
+					if k4 == k1 {
+						s := []string{k1, k2, k3}
+						slices.Sort(s)
+						k := strings.Join(s, ",")
+						clusters[k] = s
+					}
+				}
+			}
+		}
+	}
+
+	return clusters
 }
--- advent-of-code-2024/day23/task_test.go
+++ advent-of-code-2024/day23/task_test.go
@@ -8,11 +8,44 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 7, part1(testInput))
+	require.Equal(t, 1238, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, "co,de,ka,ta", part2(testInput))
+	require.Equal(t, "bg,bl,ch,fn,fv,gd,jn,kk,lk,pv,rr,tb,vw", part2(utils.GetInput(t, "input.txt")))
 }
+
+const testInput = `kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn`
