eb32a8030523bd9cbeefd72f5d6517586fcc670a
morten <morten.sogard.jensen@gmail.com>
2024-12-23 14:34:47 +0000

    day 22

--- advent-of-code-2024/day22/task.go
+++ advent-of-code-2024/day22/task.go
@@ -2,19 +2,74 @@ package day22
 
 import (
 	"fmt"
-	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2024/utils"
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	s := 0
+	for _, n := range utils.GetAllNumbers(input) {
+		for range 2000 {
+			n = nextSecretNumber(n)
+		}
+
+		s += n
 	}
-	return 0
+
+	return s
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	changes := map[string]int{}
+
+	for _, n := range utils.GetAllNumbers(input) {
+		bananas := map[int]int{}
+		for i := range 2000 {
+			n = nextSecretNumber(n)
+			bananas[i] = n % 10
+		}
+
+		thisChanges := map[string]int{}
+		for i := 4; i < len(bananas); i++ {
+			c1 := bananas[i-1] - bananas[i-0]
+			c2 := bananas[i-2] - bananas[i-1]
+			c3 := bananas[i-3] - bananas[i-2]
+			c4 := bananas[i-4] - bananas[i-3]
+			key := fmt.Sprintf("%d,%d,%d,%d", c4, c3, c2, c1)
+			if _, e := thisChanges[key]; !e {
+				thisChanges[key] = bananas[i]
+			}
+		}
+
+		for k, v := range thisChanges {
+			changes[k] += v
+		}
 	}
-	return 0
+
+	highest := 0
+	for _, v := range changes {
+		highest = max(highest, v)
+	}
+
+	return highest
+}
+
+func nextSecretNumber(n int) int {
+	n = mix(n, n*64)
+	n = prune(n)
+
+	n = mix(n, n/32)
+	n = prune(n)
+
+	n = mix(n, n*2048)
+	n = prune(n)
+	return n
+}
+
+func mix(n, r int) int {
+	return r ^ n
+}
+
+func prune(n int) int {
+	return n % 16777216
 }
--- advent-of-code-2024/day22/task_test.go
+++ advent-of-code-2024/day22/task_test.go
@@ -8,11 +8,17 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 37327623, part1(`1
+10
+100
+2024`))
+	require.Equal(t, 16953639210, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 23, part2(`1
+2
+3
+2024`))
+	require.Equal(t, 1863, part2(utils.GetInput(t, "input.txt")))
 }
