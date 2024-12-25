b1d51eaf96ce69cca185a567a45a480731d5e7d2
morten <morten.sogard.jensen@gmail.com>
2024-12-25 10:31:05 +0000

    day 25

--- advent-of-code-2024/day25/task.go
+++ advent-of-code-2024/day25/task.go
@@ -6,10 +6,49 @@ import (
 )
 
 func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+	blocks := strings.Split(input, "\n\n")
+
+	keys := [][5]int{}
+	locks := [][5]int{}
+	for _, block := range blocks {
+		lines := strings.Split(block, "\n")
+
+		teeth := [5]int{}
+		for x := range 5 {
+			t := 0
+			for y := 1; y < 6; y++ {
+				if lines[y][x] == '#' {
+					t += 1
+				}
+			}
+			teeth[x] = t
+		}
+
+		if lines[0][0] == '#' {
+			locks = append(locks, teeth)
+		} else {
+			keys = append(keys, teeth)
+		}
 	}
-	return 0
+
+	combis := 0
+	for _, k := range keys {
+		for _, l := range locks {
+			fits := true
+			for i := range len(k) {
+				if k[i]+l[i] > 5 {
+					fits = false
+					break
+				}
+			}
+
+			if fits {
+				combis += 1
+			}
+		}
+	}
+
+	return combis
 }
 
 func part2(input string) int {
--- advent-of-code-2024/day25/task_test.go
+++ advent-of-code-2024/day25/task_test.go
@@ -8,8 +8,46 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 3, part1(`#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####`))
+	require.Equal(t, 3483, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
